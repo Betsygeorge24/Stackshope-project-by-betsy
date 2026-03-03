9b7fbc0d34e25c208118f8819a2076c58d057853
Hari <vharikrishnan532@gmail.com.com>
2026-03-03 14:36:34 +0530

    admin added

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('admin_dashboard', views.admin_dashboard_view, name='admin_dashboard'),
 ]
--- admin_app/views.py
+++ admin_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def admin_dashboard_view(request):
+    return render(request,'admindashboard.html')
 # Create your views here.
