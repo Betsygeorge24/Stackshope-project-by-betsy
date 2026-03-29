da9f544fbf4d05b50d26af8548c6ef1c029e4378
Betsy <betsygeorge1124@gmail.com.com>
2026-03-29 21:16:08 +0530

    Updated settings

--- StackShop_project/settings.py
+++ StackShop_project/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment-checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-1234567890abcdef'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() in ('1', 'true', 'yes')
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
